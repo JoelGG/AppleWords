562d1819b12fba742247d6c440d48bb67e7677e1
Joel Grimmer <joel.grimmer@gmail.com>
2021-03-21 17:22:13 +0000

    spring boot init

---
+++ applewords-backend/src/main/java/com/joelgrimmer/applewordsbackend/ApplewordsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.joelgrimmer.applewordsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplewordsBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApplewordsBackendApplication.class, args);
+  }
+
+}
